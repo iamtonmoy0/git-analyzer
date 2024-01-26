29d18eb4e2892dd34b50663da820126d6381afe5
iamtonmoy0 <tonmoy2code@gmail.com>
2024-01-26 20:52:42 +0600

    search bar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
+"use client";
 export default function Home() {
+  // handle click
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("clicked");
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[18081f]">
-      this is the content
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-[18081f]">
+      <form className="relative" onSubmit={(e) => handleClick(e)}>
+        <label className="sr-only"> Search </label>
+
+        <input
+          className="h-10 w-[400px] rounded-full text-gray-300 border-none bg-gray-600 pe-10 ps-4 text-sm shadow-sm "
+          id="search"
+          type="search"
+          placeholder="Enter Github Username ..."
+        />
+
+        <button
+          type="submit"
+          className="absolute end-1 top-1/2 -translate-y-1/2 rounded-full bg-gray-50 p-2 text-gray-600 transition hover:text-gray-700"
+        >
+          <span className="sr-only">Search</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </form>
     </main>
   );
 }
